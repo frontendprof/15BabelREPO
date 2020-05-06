38a9f8c9939058ccc4fa96427394b9c4107895bb
AbdulMalik <abdumaliksharipov@gmail.com>
2020-05-06 08:58:08 -0500

    Logic for tweak added

---
+++ app.js
@@ -0,0 +1,14 @@
+const colors = ["green", "red", "cyan", "red", "yellow","magenta","coral", "rgba(133,122,200)", "#f15025"];
+
+
+const btn=document.getElementById("btn");
+
+const color=document.querySelector(".color");
+
+btn.addEventListener("click",function(){
+    let randomColor=Math.floor(Math.random()*colors.length);
+    console.log(randomColor);
+    
+    document.body.style.backgroundColor=colors[randomColor];
+    color.textContent=colors[randomColor];
+})
