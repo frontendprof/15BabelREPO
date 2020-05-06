2c3c452e149ee19468b2c6f6a07793a8a81067c2
AbdulMalik <abdumaliksharipov@gmail.com>
2020-05-06 09:34:16 -0500

    Logic on both pages complete

--- hex.js
+++ hex.js
@@ -4,18 +4,17 @@ const hexes = ["1", "2", "3", "4", "5","6","7","8", "9","A","B","C","D","E"];
 const btn=document.getElementById("btn");
 const color=document.querySelector(".color");
 
-btn.addEventListener('click',randomHexFunc());
 
-let hexValue=Math.floor(Math.random*hexes.length);
 
-function randomHexFunc(){
+btn.addEventListener('click', function(){
     let hash="#";
 
     for(let i=0;i<6;i++){
+        let hexValue=Math.floor(Math.random()*hexes.length);
         hash+=hexes[hexValue];
     }
 
     document.body.style.backgroundColor=hash;
     color.textContent=hash;
-}
 
+});
